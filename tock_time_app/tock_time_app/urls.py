ffb57957206373703098f41530052637c9ba1f21
Lazarina Batcheva <lazarina.batcheva@gmail.com>
2024-11-19 12:10:10 +0100

    feat: create 3 new apps (common, accounts, tasks)

---
+++ tock_time_app/tock_time_app/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('tock_time_app.common.urls')),
+    path('accounts/', include('tock_time_app.accounts.urls')),
+    path('tasks/', include('tock_time_app.tasks.urls')),
+]
