8b7a7b069655bf9bde6a0ca2cef7390cfca153a8
Lazarina Batcheva <lazarina.batcheva@gmail.com>
2024-11-19 21:33:35 +0100

    feat(accounts): add user Admin site
    - Create AppUserCreationForm for user creation
    - Create AppUserChangeForm for user updates
    - Register and customize AppUserAdmin for the admin panel

--- tock_time_app/tock_time_app/accounts/admin.py
+++ tock_time_app/tock_time_app/accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from tock_time_app.accounts.forms import AppUserCreationForm, AppUserChangeForm
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class AppUserAdmin(UserAdmin):
+    model = UserModel
+    add_form = AppUserCreationForm
+    form = AppUserChangeForm
+
+    list_display = ['username', 'email', 'is_staff', 'is_superuser']
+    readonly_fields = ['date_joined', ]
+    search_fields = ['username', 'email']
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email', )}),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': ('username', 'email', 'password1', 'password2'),
+            },
+        ),
+    )
